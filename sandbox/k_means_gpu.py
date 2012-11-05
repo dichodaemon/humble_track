15bf7cd7284c57978751fd65556899c16fd00111
Dizan Vasquez <dix_ans@fastmail.fm>
2012-11-05 19:14:43 +0100

    Add profiling code for gpu

--- sandbox/k_means_gpu.py
+++ sandbox/k_means_gpu.py
@@ -1,5 +1,6 @@
 import pyopencl as cl
 import numpy as np
+from tick_tack import *
 
 import util
 
@@ -9,12 +10,15 @@ class _expectation( util.Program ):
     self.kmeans = self.loadProgram( "kmeans.cl" )
 
   def __call__( self, data, centroids ):
+    tick( "Preparation" )
     mf = cl.mem_flags
 
     assignments = np.zeros( ( data.shape[0], ), np.int32 )
     assignments_b = cl.Buffer( self.context, mf.WRITE_ONLY, assignments.nbytes )
     data_b = cl.Buffer( self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf = data )
     centroids_b = cl.Buffer( self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf = centroids )
+    tack( "Preparation" )
+    tick( "Execution" )
 
     self.kmeans.expectation(
       self.queue, ( data.shape[0], ), None,
@@ -23,6 +27,7 @@ class _expectation( util.Program ):
     )
 
     cl.enqueue_copy( self.queue, assignments, assignments_b )
+    tack( "Execution" )
     return assignments
 
 
